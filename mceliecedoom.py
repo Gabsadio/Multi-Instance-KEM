8e5a81a558cfa61fc3c04af1f8e96f910f05653c
Gabriel Sá Diogo <gabriel.sadiogo@edu.rub.de>
2026-05-15 12:14:30 +0200

    Recalculated complexities of 1-Out-of-M McEliece session key recovery

--- mceliecedoom.py
+++ mceliecedoom.py
@@ -1,13 +1,11 @@
 from doom import dsDOOM, mmtDOOM
 from matplotlib import pyplot as plt
+from tqdm import tqdm
 import istarmap
 from multiprocessing.pool import Pool
 from multiprocessing import cpu_count
-from tqdm import tqdm
-
 
 if __name__ == "__main__":
-    plt.style.use("bmh")
     mceliece_parameters = [
         # First two not part of ISO spec
         (3488, 12, 64),
@@ -16,48 +14,42 @@ if __name__ == "__main__":
         (6960, 13, 119),
         (8192, 13, 128),
     ]
-    for n, m, t in mceliece_parameters:
-        print(f"mceliece-{n}{t} ...")
-        k = n - m * t
+    for n, m, w in mceliece_parameters:
+        print(f"mceliece{n}{w} ...")
+        k = n - m * w
 
-        # Single-instance:
-        print("Single-instance:\nDS: ", end="")
-        timeds, memds, (mmds, pds, lds, p_ds, kl_ds) = dsDOOM(n, k - 1, t, 1)
-        print(timeds, "\nMMT: ", end="")
-        timemmt, memmmt, (mmmmt, pmmt, lmmt, b, rkl, p_mmt, rp_) = mmtDOOM(
-            n, k - 1, t, 1
-        )
+        # Single-Instance:
+        print("Single-Instance:\nDS: ", end="")
+        timeds, memds, (mmds, pds, lds, rds) = dsDOOM(n, k - 1, w, 1)
+        print(timeds)
+        print("MMT: ", end="")
+        timemmt, memmmt, (mmmmt, pmmt, lmmt, bmmt, p_, rmmt) = mmtDOOM(n, k - 1, w, 1)
         print(timemmt)
 
         # A little more than 2/3*log2(T_1) to make sure we get to the minimal time
         instanceRange = range(int(3 / 4 * max(timeds, timemmt)))
-
         DSruntimes = dict((i, timeds) for i in instanceRange)
         DSspeedups = dict((i, float(0)) for i in instanceRange)
         DSmemory = dict((i, memds) for i in instanceRange)
         DSsyndromesUsed = dict((i, mmds) for i in instanceRange)
         DSparameters_p = dict((i, pds) for i in instanceRange)
         DSparameters_l = dict((i, lds) for i in instanceRange)
-        DSparameters_p1 = dict((i, p_ds) for i in instanceRange)
-        DSparameters_l1 = dict((i, kl_ds) for i in instanceRange)
+        DSparameters_r = dict((i, rds) for i in instanceRange)
         MMTruntimes = dict((i, timemmt) for i in instanceRange)
         MMTspeedups = dict((i, float(0)) for i in instanceRange)
         MMTmemory = dict((i, memmmt) for i in instanceRange)
         MMTsyndromesUsed = dict((i, mmmmt) for i in instanceRange)
         MMTparameters_p = dict((i, pmmt) for i in instanceRange)
         MMTparameters_l = dict((i, lmmt) for i in instanceRange)
-        MMTparameters_b = dict((i, b) for i in instanceRange)
-        MMTparameters_l2 = dict((i, rkl) for i in instanceRange)
-        MMTparameters_p1 = dict((i, p_mmt) for i in instanceRange)
-        MMTparameters_p12 = dict((i, rp_) for i in instanceRange)
+        MMTparameters_b = dict((i, bmmt) for i in instanceRange)
+        MMTparameters_p_ = dict((i, p_) for i in instanceRange)
+        MMTparameters_r = dict((i, rmmt) for i in instanceRange)
 
-        # Multi-instance:
+        # Multi-Instance:
         print("Multi-Instance:")
         with Pool(processes=min(64, cpu_count())) as pool:
-            input = [(n, k - 1, t, 1 << i) for i in instanceRange[1:]]
-            print("DS:")
+            input = [(n, k - 1, w, 1 << i) for i in instanceRange[1:]]
             DSresults = list(tqdm(pool.istarmap(dsDOOM, input), total=len(input)))
-            print("MMT:")
             MMTresults = list(tqdm(pool.istarmap(mmtDOOM, input), total=len(input)))
 
         for i in instanceRange[1:]:
@@ -68,8 +60,7 @@ if __name__ == "__main__":
                     DSsyndromesUsed[i],
                     DSparameters_p[i],
                     DSparameters_l[i],
-                    DSparameters_p1[i],
-                    DSparameters_l1[i],
+                    DSparameters_r[i],
                 ),
             ) = DSresults[i - 1]
             if DSruntimes[i] > DSruntimes[i - 1]:
@@ -78,8 +69,7 @@ if __name__ == "__main__":
                 DSsyndromesUsed[i] = DSsyndromesUsed[i - 1]
                 DSparameters_p[i] = DSparameters_p[i - 1]
                 DSparameters_l[i] = DSparameters_l[i - 1]
-                DSparameters_p1[i] = DSparameters_p1[i - 1]
-                DSparameters_l1[i] = DSparameters_l1[i - 1]
+                DSparameters_r[i] = DSparameters_r[i - 1]
             DSspeedups[i] = (DSruntimes[0] - DSruntimes[i]) / i
 
             (
@@ -90,9 +80,8 @@ if __name__ == "__main__":
                     MMTparameters_p[i],
                     MMTparameters_l[i],
                     MMTparameters_b[i],
-                    MMTparameters_l2[i],
-                    MMTparameters_p1[i],
-                    MMTparameters_p12[i],
+                    MMTparameters_p_[i],
+                    MMTparameters_r[i],
                 ),
             ) = MMTresults[i - 1]
             if MMTruntimes[i] > MMTruntimes[i - 1]:
@@ -102,13 +91,12 @@ if __name__ == "__main__":
                 MMTparameters_p[i] = MMTparameters_p[i - 1]
                 MMTparameters_l[i] = MMTparameters_l[i - 1]
                 MMTparameters_b[i] = MMTparameters_b[i - 1]
-                MMTparameters_l2[i] = MMTparameters_l2[i - 1]
-                MMTparameters_p1[i] = MMTparameters_p1[i - 1]
-                MMTparameters_p12[i] = MMTparameters_p12[i - 1]
+                MMTparameters_p_[i] = MMTparameters_p_[i - 1]
+                MMTparameters_r[i] = MMTparameters_r[i - 1]
             MMTspeedups[i] = (MMTruntimes[0] - MMTruntimes[i]) / i
 
         # Save the results in a file:
-        with open(f"Out/McElieceDOOM/{n}{t}.txt", "w") as file:
+        with open(f"Out/McElieceDOOM/{n}{w}.txt", "w") as file:
             file.write("DS-DOOM\n")
             file.write("-------\n")
             file.write(
@@ -147,13 +135,8 @@ if __name__ == "__main__":
                 + "\n"
             )
             file.write(
-                "p_                    :\t"
-                + "\t".join(f"{entry:20d}" for entry in DSparameters_p1.values())
-                + "\n"
-            )
-            file.write(
-                "kl_                   :\t"
-                + "\t".join(f"{entry:20d}" for entry in DSparameters_l1.values())
+                "r                     :\t"
+                + "\t".join(f"{entry:20.16f}" for entry in DSparameters_r.values())
                 + "\n"
             )
 
@@ -200,52 +183,48 @@ if __name__ == "__main__":
                 + "\n"
             )
             file.write(
-                "rkl                   :\t"
-                + "\t".join(f"{entry:20d}" for entry in MMTparameters_l2.values())
-                + "\n"
-            )
-            file.write(
                 "p_                    :\t"
-                + "\t".join(f"{entry:20d}" for entry in MMTparameters_p1.values())
+                + "\t".join(f"{entry:20.16f}" for entry in MMTparameters_p_.values())
                 + "\n"
             )
             file.write(
-                "rp_                   :\t"
-                + "\t".join(f"{entry:20d}" for entry in MMTparameters_p12.values())
+                "r                     :\t"
+                + "\t".join(f"{entry:20.16f}" for entry in MMTparameters_r.values())
                 + "\n"
             )
 
         # Plot the results:
-        plt.figure()
-        plt.xlabel("$\\log_2(M)$")
-        plt.ylabel("$\\log_2$ runtime")
-        plt.plot(DSruntimes.keys(), DSruntimes.values(), label="DS")
-        plt.plot(MMTruntimes.keys(), MMTruntimes.values(), label="MMT")
-        plt.legend()
-        y = (
-            143 * (f"{n}{t}" == "348864")
-            + 207 * (f"{n}{t}" == "460896")
-            + 272 * (f"{n}{t}" in ["6688128", "6960119", "8192128"])
-        )
-        plt.axhline(y, color="tab:gray", ls="--")
-        plt.savefig(f"Figures/McElieceDOOM/{n}{t}/Runtimes")
+        plt.style.use("bmh")
+        plt.rcParams.update({"backend": "pgf", "pgf.texsystem": "pdflatex"})
 
         plt.figure()
+        plt.plot(DSruntimes.keys(), DSruntimes.values(), label="DS-DOOM")
+        plt.plot(MMTruntimes.keys(), MMTruntimes.values(), label="MMT-DOOM")
+        y = 143 * (n == 3488) + 207 * (n == 4608) + 272 * (n in [6688, 6960, 8192])
+        plt.axhline(y, color="tab:gray", ls="--")
+        plt.legend()
         plt.xlabel("$\\log_2(M)$")
-        plt.ylabel("$\\log_M$ speedups")
-        plt.plot(list(DSspeedups.keys())[1:], list(DSspeedups.values())[1:], label="DS")
+        plt.ylabel("$\\log_2$ runtime")
+        plt.savefig(f"Figures/McElieceDOOM/{n}{w}/Runtimes")
+
+        plt.figure()
+        plt.plot(
+            list(DSspeedups.keys())[1:], list(DSspeedups.values())[1:], label="DS-DOOM"
+        )
         plt.plot(
             list(MMTspeedups.keys())[1:],
             list(MMTspeedups.values())[1:],
-            label="MMT",
+            label="MMT-DOOM",
         )
         plt.legend()
-        plt.savefig(f"Figures/McElieceDOOM/{n}{t}/Speedups")
+        plt.xlabel("$\\log_2(M)$")
+        plt.ylabel("$\\log_M$ speedups")
+        plt.savefig(f"Figures/McElieceDOOM/{n}{w}/Speedups")
 
         plt.figure()
+        plt.plot(DSmemory.keys(), DSmemory.values(), label="DS-DOOM")
+        plt.plot(MMTmemory.keys(), MMTmemory.values(), label="MMT-DOOM")
+        plt.legend()
         plt.xlabel("$\\log_2(M)$")
         plt.ylabel("$\\log_2$ memory")
-        plt.plot(DSmemory.keys(), DSmemory.values(), label="DS")
-        plt.plot(MMTmemory.keys(), MMTmemory.values(), label="MMT")
-        plt.legend()
-        plt.savefig(f"Figures/McElieceDOOM/{n}{t}/Memory")
+        plt.savefig(f"Figures/McElieceDOOM/{n}{w}/Memory")
