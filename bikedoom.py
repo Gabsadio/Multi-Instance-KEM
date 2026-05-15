56b8559f7f715941a3c6e069b32f9e5f7aab677f
Gabriel Sá Diogo <gabriel.sadiogo@edu.rub.de>
2026-05-15 12:12:19 +0200

    Recalculated complexities of 1-Out-of-M BIKE session key attack

--- bikedoom.py
+++ bikedoom.py
@@ -1,104 +1,94 @@
 from doom import dsDOOM, mmtDOOM
 from math import log2
 from matplotlib import pyplot as plt
+from tqdm import tqdm
 import istarmap
 from multiprocessing.pool import Pool
 from multiprocessing import cpu_count
-from tqdm import tqdm
-
 
 if __name__ == "__main__":
-    plt.style.use("bmh")
     bike_parameters = [(12323, 134), (24659, 199), (40973, 264)]
-    for j, (r, t) in enumerate(bike_parameters):
-        print(f"BIKE-{2*j+1}...")
+    for j, (n, w) in enumerate(bike_parameters):
+        print(f"BIKE-{2*j+1} ...")
 
-        # Single-instance:
-        print("Single instance:\nDS: ", end="")
-        time, mem, (mm, p, l, p_, kl_) = dsDOOM(2 * r, r - 1, t, 1 * r)
-        print(time, "\nMMT: ", end="")
-        timemmt, memmmt, (mmmmt, pmmt, lmmt, b, rkl, p_mmt, rp_) = mmtDOOM(
-            2 * r, r - 1, t, 1 * r
+        # Single-Instance:
+        print("Single-Instace:\nDS: ", end="")
+        timeds, memds, (mmds, pds, lds, rds) = dsDOOM(2 * n, n - 1, w, n)
+        print(timeds)
+        print("MMT: ", end="")
+        timemmt, memmmt, (mmmmt, pmmt, lmmt, bmmt, p_, rmmt) = mmtDOOM(
+            2 * n, n - 1, w, n
         )
         print(timemmt)
 
         # A little more than 2/3*log2(T_1) to make sure we get to the minimal time
-        instanceRange = range(int(3 / 4 * max(time, timemmt) - log2(r)))
+        instanceRange = range(int(3 / 4 * max(timeds, timemmt) - log2(n)))
+        DSruntimes = dict((i, timeds) for i in instanceRange)
+        DSspeedups = dict((i, float(0)) for i in instanceRange)
+        DSmemory = dict((i, memds) for i in instanceRange)
+        DSsyndromesUsed = dict((i, mmds) for i in instanceRange)
+        DSparameters_p = dict((i, pds) for i in instanceRange)
+        DSparameters_l = dict((i, lds) for i in instanceRange)
+        DSparameters_r = dict((i, rds) for i in instanceRange)
+        MMTruntimes = dict((i, timemmt) for i in instanceRange)
+        MMTspeedups = dict((i, float(0)) for i in instanceRange)
+        MMTmemory = dict((i, memmmt) for i in instanceRange)
+        MMTsyndromesUsed = dict((i, mmmmt) for i in instanceRange)
+        MMTparameters_p = dict((i, pmmt) for i in instanceRange)
+        MMTparameters_l = dict((i, lmmt) for i in instanceRange)
+        MMTparameters_b = dict((i, bmmt) for i in instanceRange)
+        MMTparameters_p_ = dict((i, p_) for i in instanceRange)
+        MMTparameters_r = dict((i, rmmt) for i in instanceRange)
 
-        dsruntimes = dict((i, time) for i in instanceRange)
-        dsspeedups = dict((i, float(0)) for i in instanceRange)
-        dsmemory = dict((i, mem) for i in instanceRange)
-        dssyndromesUsed = dict((i, mm) for i in instanceRange)
-        dsparameters_p = dict((i, p) for i in instanceRange)
-        dsparameters_l = dict((i, l) for i in instanceRange)
-        dsparameters_p_ = dict((i, p_) for i in instanceRange)
-        dsparameters_kl_ = dict((i, kl_) for i in instanceRange)
-        mmtruntimes = dict((i, timemmt) for i in instanceRange)
-        mmtspeedups = dict((i, float(0)) for i in instanceRange)
-        mmtmemory = dict((i, memmmt) for i in instanceRange)
-        mmtsyndromesUsed = dict((i, mmmmt) for i in instanceRange)
-        mmtparameters_p = dict((i, pmmt) for i in instanceRange)
-        mmtparameters_l = dict((i, lmmt) for i in instanceRange)
-        mmtparameters_b = dict((i, b) for i in instanceRange)
-        mmtparameters_rkl = dict((i, rkl) for i in instanceRange)
-        mmtparameters_p_ = dict((i, p_mmt) for i in instanceRange)
-        mmtparameters_rp_ = dict((i, rp_) for i in instanceRange)
-
-        # Multi-instance:
+        # Multi-Instance:
         print("Multi-Instance:")
         with Pool(processes=min(64, cpu_count())) as pool:
-            input = [(2 * r, r - 1, t, (1 << i) * r) for i in instanceRange[1:]]
-            print("DS:")
-            dsresults = list(tqdm(pool.istarmap(dsDOOM, input), total=len(input)))
-            print("MMT:")
-            mmtresults = list(tqdm(pool.istarmap(mmtDOOM, input), total=len(input)))
+            input = [(2 * n, n - 1, w, n * (1 << i)) for i in instanceRange[1:]]
+            DSresults = list(tqdm(pool.istarmap(dsDOOM, input), total=len(input)))
+            MMTresults = list(tqdm(pool.istarmap(mmtDOOM, input), total=len(input)))
 
         for i in instanceRange[1:]:
             (
-                dsruntimes[i],
-                dsmemory[i],
+                DSruntimes[i],
+                DSmemory[i],
                 (
-                    dssyndromesUsed[i],
-                    dsparameters_p[i],
-                    dsparameters_l[i],
-                    dsparameters_p_[i],
-                    dsparameters_kl_[i],
+                    DSsyndromesUsed[i],
+                    DSparameters_p[i],
+                    DSparameters_l[i],
+                    DSparameters_r[i],
                 ),
-            ) = dsresults[i - 1]
-            if dsruntimes[i] > dsruntimes[i - 1]:
-                dsruntimes[i] = dsruntimes[i - 1]
-                dsmemory[i] = dsmemory[i - 1]
-                dssyndromesUsed[i] = dssyndromesUsed[i - 1]
-                dsparameters_p[i] = dsparameters_p[i - 1]
-                dsparameters_l[i] = dsparameters_l[i - 1]
-                dsparameters_p_[i] = dsparameters_p_[i - 1]
-                dsparameters_kl_[i] = dsparameters_kl_[i - 1]
-            dsspeedups[i] = (dsruntimes[0] - dsruntimes[i]) / i
+            ) = DSresults[i - 1]
+            if DSruntimes[i] > DSruntimes[i - 1]:
+                DSruntimes[i] = DSruntimes[i - 1]
+                DSmemory[i] = DSmemory[i - 1]
+                DSsyndromesUsed[i] = DSsyndromesUsed[i - 1]
+                DSparameters_p[i] = DSparameters_p[i - 1]
+                DSparameters_l[i] = DSparameters_l[i - 1]
+                DSparameters_r[i] = DSparameters_r[i - 1]
+            DSspeedups[i] = (DSruntimes[0] - DSruntimes[i]) / i
 
             (
-                mmtruntimes[i],
-                mmtmemory[i],
+                MMTruntimes[i],
+                MMTmemory[i],
                 (
-                    mmtsyndromesUsed[i],
-                    mmtparameters_p[i],
-                    mmtparameters_l[i],
-                    mmtparameters_b[i],
-                    mmtparameters_rkl[i],
-                    mmtparameters_p_[i],
-                    mmtparameters_rp_[i],
+                    MMTsyndromesUsed[i],
+                    MMTparameters_p[i],
+                    MMTparameters_l[i],
+                    MMTparameters_b[i],
+                    MMTparameters_p_[i],
+                    MMTparameters_r[i],
                 ),
-            ) = mmtresults[i - 1]
-            if mmtruntimes[i] > mmtruntimes[i - 1]:
-                mmtruntimes[i] = mmtruntimes[i - 1]
-                mmtmemory[i] = mmtmemory[i - 1]
-                mmtsyndromesUsed[i] = mmtsyndromesUsed[i - 1]
-                mmtparameters_p[i] = mmtparameters_p[i - 1]
-                mmtparameters_l[i] = mmtparameters_l[i - 1]
-                mmtparameters_b[i] = mmtparameters_b[i - 1]
-                mmtparameters_rkl[i] = mmtparameters_rkl[i - 1]
-                mmtparameters_p_[i] = mmtparameters_p_[i - 1]
-                mmtparameters_rp_[i] = mmtparameters_rp_[i - 1]
-            mmtspeedups[i] = (mmtruntimes[0] - mmtruntimes[i]) / i
+            ) = MMTresults[i - 1]
+            if MMTruntimes[i] > MMTruntimes[i - 1]:
+                MMTruntimes[i] = MMTruntimes[i - 1]
+                MMTmemory[i] = MMTmemory[i - 1]
+                MMTsyndromesUsed[i] = MMTsyndromesUsed[i - 1]
+                MMTparameters_p[i] = MMTparameters_p[i - 1]
+                MMTparameters_l[i] = MMTparameters_l[i - 1]
+                MMTparameters_b[i] = MMTparameters_b[i - 1]
+                MMTparameters_p_[i] = MMTparameters_p_[i - 1]
+                MMTparameters_r[i] = MMTparameters_r[i - 1]
+            MMTspeedups[i] = (MMTruntimes[0] - MMTruntimes[i]) / i
 
         # Save the results in a file:
         with open(f"Out/BIKEDOOM/BIKE-{2*j+1}.txt", "w") as file:
@@ -111,42 +101,37 @@ if __name__ == "__main__":
             )
             file.write(
                 "log2 # syndromes used :\t"
-                + "\t".join(f"{entry:20.16f}" for entry in dssyndromesUsed.values())
+                + "\t".join(f"{entry:20.16f}" for entry in DSsyndromesUsed.values())
                 + "\n"
             )
             file.write(
                 "Runtime               :\t"
-                + "\t".join(f"{entry:20.16f}" for entry in dsruntimes.values())
+                + "\t".join(f"{entry:20.16f}" for entry in DSruntimes.values())
                 + "\n"
             )
             file.write(
                 "Speedups              :\t"
-                + "\t".join(f"{entry:20.16f}" for entry in dsspeedups.values())
+                + "\t".join(f"{entry:20.16f}" for entry in DSspeedups.values())
                 + "\n"
             )
             file.write(
                 "Memory                :\t"
-                + "\t".join(f"{entry:20.16f}" for entry in dsmemory.values())
+                + "\t".join(f"{entry:20.16f}" for entry in DSmemory.values())
                 + "\n"
             )
             file.write(
                 "p                     :\t"
-                + "\t".join(f"{entry:20d}" for entry in dsparameters_p.values())
+                + "\t".join(f"{entry:20d}" for entry in DSparameters_p.values())
                 + "\n"
             )
             file.write(
                 "l                     :\t"
-                + "\t".join(f"{entry:20d}" for entry in dsparameters_l.values())
+                + "\t".join(f"{entry:20d}" for entry in DSparameters_l.values())
                 + "\n"
             )
             file.write(
-                "p_                    :\t"
-                + "\t".join(f"{entry:20d}" for entry in dsparameters_p_.values())
-                + "\n"
-            )
-            file.write(
-                "rkl                   :\t"
-                + "\t".join(f"{entry:20d}" for entry in dsparameters_kl_.values())
+                "r                     :\t"
+                + "\t".join(f"{entry:20.16f}" for entry in DSparameters_r.values())
                 + "\n"
             )
 
@@ -159,80 +144,82 @@ if __name__ == "__main__":
             )
             file.write(
                 "log2 # syndromes used :\t"
-                + "\t".join(f"{entry:20.16f}" for entry in mmtsyndromesUsed.values())
+                + "\t".join(f"{entry:20.16f}" for entry in MMTsyndromesUsed.values())
                 + "\n"
             )
             file.write(
                 "Runtime               :\t"
-                + "\t".join(f"{entry:20.16f}" for entry in mmtruntimes.values())
+                + "\t".join(f"{entry:20.16f}" for entry in MMTruntimes.values())
                 + "\n"
             )
             file.write(
                 "Speedups              :\t"
-                + "\t".join(f"{entry:20.16f}" for entry in mmtspeedups.values())
+                + "\t".join(f"{entry:20.16f}" for entry in MMTspeedups.values())
                 + "\n"
             )
             file.write(
                 "Memory                :\t"
-                + "\t".join(f"{entry:20.16f}" for entry in mmtmemory.values())
+                + "\t".join(f"{entry:20.16f}" for entry in MMTmemory.values())
                 + "\n"
             )
             file.write(
                 "p                     :\t"
-                + "\t".join(f"{entry:20d}" for entry in mmtparameters_p.values())
+                + "\t".join(f"{entry:20d}" for entry in MMTparameters_p.values())
                 + "\n"
             )
             file.write(
                 "l                     :\t"
-                + "\t".join(f"{entry:20d}" for entry in mmtparameters_l.values())
+                + "\t".join(f"{entry:20d}" for entry in MMTparameters_l.values())
                 + "\n"
             )
             file.write(
                 "b                     :\t"
-                + "\t".join(f"{entry:20d}" for entry in mmtparameters_b.values())
-                + "\n"
-            )
-            file.write(
-                "rkl                   :\t"
-                + "\t".join(f"{entry:20d}" for entry in mmtparameters_rkl.values())
+                + "\t".join(f"{entry:20d}" for entry in MMTparameters_b.values())
                 + "\n"
             )
             file.write(
                 "p_                    :\t"
-                + "\t".join(f"{entry:20d}" for entry in mmtparameters_p_.values())
+                + "\t".join(f"{entry:20.16f}" for entry in MMTparameters_p_.values())
                 + "\n"
             )
             file.write(
-                "rp_                   :\t"
-                + "\t".join(f"{entry:20d}" for entry in mmtparameters_rp_.values())
+                "r                     :\t"
+                + "\t".join(f"{entry:20.16f}" for entry in MMTparameters_r.values())
                 + "\n"
             )
 
         # Plot the results:
+        plt.style.use("bmh")
+        plt.rcParams.update({"backend": "pgf", "pgf.texsystem": "pdflatex"})
+
         plt.figure()
-        plt.xlabel("$\\log_2(M)$")
-        plt.ylabel("$\\log_2$ runtime")
-        plt.plot(dsruntimes.keys(), dsruntimes.values(), label="DS")
-        plt.plot(mmtruntimes.keys(), mmtruntimes.values(), label="MMT")
-        plt.legend()
+        plt.plot(DSruntimes.keys(), DSruntimes.values(), label="DS-DOOM")
+        plt.plot(MMTruntimes.keys(), MMTruntimes.values(), label="MMT-DOOM")
         y = 143 * (j == 0) + 207 * (j == 1) + 272 * (j == 2)
         plt.axhline(y, color="tab:gray", ls="--")
+        plt.legend()
+        plt.xlabel("$\\log_2(M)$")
+        plt.ylabel("$\\log_2$ runtime")
         plt.savefig(f"Figures/BIKEDOOM/BIKE-{2*j+1}/Runtimes")
 
         plt.figure()
-        plt.xlabel("$\\log_2(M)$")
-        plt.ylabel("$\\log_M$ speedups")
-        plt.plot(list(dsspeedups.keys())[1:], list(dsspeedups.values())[1:], label="DS")
         plt.plot(
-            list(mmtspeedups.keys())[1:], list(mmtspeedups.values())[1:], label="MMT"
+            list(DSspeedups.keys())[1:], list(DSspeedups.values())[1:], label="DS-DOOM"
+        )
+        plt.plot(
+            list(MMTspeedups.keys())[1:],
+            list(MMTspeedups.values())[1:],
+            label="MMT-DOOM",
         )
         plt.legend()
+        plt.xlabel("$\\log_2(M)$")
+        plt.ylabel("$\\log_M$ speedups")
         plt.savefig(f"Figures/BIKEDOOM/BIKE-{2*j+1}/Speedups")
 
         plt.figure()
+        plt.plot(DSmemory.keys(), DSmemory.values(), label="DS-DOOM")
+        plt.plot(MMTmemory.keys(), MMTmemory.values(), label="MMT-DOOM")
+        plt.legend()
         plt.xlabel("$\\log_2(M)$")
         plt.ylabel("$\\log_2$ memory")
-        plt.plot(dsmemory.keys(), dsmemory.values(), label="DS")
-        plt.plot(mmtmemory.keys(), mmtmemory.values(), label="MMT")
-        plt.legend()
         plt.savefig(f"Figures/BIKEDOOM/BIKE-{2*j+1}/Memory")
